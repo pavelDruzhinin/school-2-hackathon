f4b55c0719645a044bd8a2e464701644ef071c2c
MADSEN\svercosh <madsen08@mail.ru>
2017-07-14 20:14:15 +0300

    Добавлен личый кабинет. Что реализованно, напишу в trello

--- RosCottedge/RosCottedge/Controllers/AccountController.cs
+++ RosCottedge/RosCottedge/Controllers/AccountController.cs
@@ -61,8 +61,8 @@ namespace RosCottedge.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(Register registerModel)
         {
-            if(User.Identity.IsAuthenticated==true)
-            return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            if (User.Identity.IsAuthenticated == true)
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
             if (ModelState.IsValid)//если модель проходит валидацию, то в базе ищем логин
             {
 
@@ -111,6 +111,8 @@ namespace RosCottedge.Controllers
         }
         #endregion
 
+        //Личный кабинет
+
         #region Личная информация
         public new ActionResult Profile()
         {
@@ -127,6 +129,21 @@ namespace RosCottedge.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
             }
         }
+        public ActionResult PersonalInformation()
+        {
+            if (User.Identity.IsAuthenticated == true)
+            {
+                User user = (from u in db.Users
+                             where u.Login == User.Identity.Name
+                             select u).FirstOrDefault();
+                return View(user);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            }
+        }
+
         #endregion
 
         #region Редактирование личной информации
@@ -165,17 +182,127 @@ namespace RosCottedge.Controllers
 
         }
         #endregion
-        public ActionResult MyHouse(House house)
+
+        #region Мои дома
+        public ActionResult MyHouse()
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                User user = db.Users.Include(h => h.Houses).Where(x => x.Login == User.Identity.Name).FirstOrDefault();
-                house.UserId = user.Id;
+            if (User.Identity.IsAuthenticated == false)
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            // Выводим все дома, которые добавлял пользователь
+            //_______________________________________________________________
+            User user = (from u in db.Users
+                         where u.Login == User.Identity.Name
+                         select u).FirstOrDefault();
 
+            IQueryable<House> house = from h in db.Houses
+                                      where h.UserId == user.Id
+                                      select h;
 
-            }
+            List<House> hou = new List<House>(house);
+            ViewBag.house = hou;
+            //Выводим дома, по которым пришла бронь 
+            //_______________________________________________________________
+
+            IQueryable<Reservation> reserv = from r in db.Reservations
+                         .Include(u => u.User)
+                                             where r.House.UserId == user.Id
+                                             select r;
+            List<Reservation> res = new List<Reservation>(reserv);
+            ViewBag.reserv = res;
+            //Выводим дома, по которым оставлен отзыв 
+            //_______________________________________________________________
+            IQueryable<Review> comment = from c in db.Reviews
+                          .Include(x => x.User).Include(x => x.House)
+                                         where c.House.UserId == user.Id
+                                         select c;
+
+            List<Review> rew = new List<Review>(comment);
+            ViewBag.rew = rew;
 
             return View();
         }
+        #endregion        
+
+        #region Редактирование моих домов
+        public ActionResult EditMyHouse(int? id)
+        {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                House house = db.Houses.Find(id);
+
+                User user = (from u in db.Users
+                             where u.Login == User.Identity.Name
+                             select u).FirstOrDefault();
+                //Вывод забронированных дат по выбранному дому
+                IQueryable<Reservation> reserv = from r in db.Reservations
+                                                .Include(x => x.User)
+                                                 where r.House.UserId == user.Id && r.HouseId == house.Id
+                                                 select r;
+
+                List<Reservation> res = new List<Reservation>(reserv);
+                ViewBag.reserv = res;
+
+                //Вывод комментариев по выбранному дому
+                IQueryable<Review> comment = from c in db.Reviews
+                                             .Include(x => x.User).Include(x => x.House)
+                                             where c.House.UserId == user.Id && c.HouseId == house.Id
+                                             select c;
+
+                List<Review> rew = new List<Review>(comment);
+                ViewBag.rew = rew;
+
+                return View(house);
+
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditMyHouse(House house)
+        {
+            if (User.Identity.IsAuthenticated == false)
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(house).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Profile");
+            }
+            return View(house);
+        }
+        #endregion
+
+        #region Мои поездки
+        public ActionResult MyTrips()
+        {
+            if (User.Identity.IsAuthenticated == false)
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            User user = (from u in db.Users
+                         where u.Login == User.Identity.Name
+                         select u).FirstOrDefault();
+
+            var reserv = from a in db.Reservations
+                         .Include(x => x.House)
+                         where a.UserId == user.Id
+                         select a;
+
+            List<Reservation> res = new List<Reservation>(reserv);
+            ViewBag.reserv = res;
+
+            //var house = from h in db.Houses
+            //            where h.
+            //            select h;
+
+
+            //var hou = new List<House>(house);
+            //ViewBag.house = hou;
+            return View();
+        }
+        #endregion
     }
 }
