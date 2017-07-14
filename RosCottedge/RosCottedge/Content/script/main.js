9ba80f6e787ea54a98d6872ca5cff361a81f5dda
DESKTOP-TCLD76E\gigdi <hoqpe@yandex.ru>
2017-07-14 05:56:31 +0300

    Фильтрция (50/50)

--- RosCottedge/RosCottedge/Content/script/main.js
+++ RosCottedge/RosCottedge/Content/script/main.js
@@ -1,13 +1,50 @@
 $(document).ready(function () {
 
     //AjaxReservation
-
     var redirectData = "";
     $('#reservation').submit(function () {
         $.post('/House/AddReservation', $('#reservation').serialize(), function (data) {
             $('.errorBox').html(data);
         });
         return false;
+    });
+
+    //Datepicker
+    $('.date').attr('readOnly','true');
+    $('.date').datepicker({
+      dateFormat: 'dd-mm-yy',
+      monthNames: [
+        "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август",
+        "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+      ],
+      dayNamesMin: [ "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" ],
+      minDate: '+1d',
+      maxDate: '+1y'
+    });
+
+    var $date1 = $('.date1');
+    var $date2 = $('.date2');
+    var val1_d, val2_d,val1_m, val2_m, val1_y, val2_y;
+    function validDate(val, date) {
+      val1_d = Number($date1.val().slice(0,2)); val2_d = Number($date2.val().slice(0,2));
+      val1_m = Number($date1.val().slice(3,5)); val2_m = Number($date2.val().slice(3,5));
+      val1_y = Number($date1.val().slice(6,10)); val2_y = Number($date2.val().slice(6,10));
+      if (val+"_d" != '' && val1_d > val2_d && val1_m == val2_m && val1_y == val2_y){
+        date.val('');
+      }else
+      if (val+"_d" != '' && val1_m > val2_m && val1_y == val2_y){
+        date.val('');
+      }else
+      if (val+"_d"!= '' && val1_y > val2_y){
+        date.val('');
+      }
+
+    }
+    $date1.change(function(){
+      validDate("val2", $date2);
+    });
+    $date2.change(function(){
+      validDate("val1", $date1);
     });
 
 });
--- RosCottedge/RosCottedge/Controllers/HomeController.cs
+++ RosCottedge/RosCottedge/Controllers/HomeController.cs
@@ -11,13 +11,43 @@ namespace RosCottedge.Controllers
     public class HomeController : Controller
     {
         private SiteContext db = new SiteContext();
-        
-        public ActionResult Index(int? page)
+
+        public ActionResult Index(int? page, string region, int? startPrice, int? finishPrice, int? numberOfPersons)
         {
             int pageNumber = (page ?? 1);
             int pageSize = 8;
-            var house = db.Houses.OrderBy(x => x.Id).ToPagedList(pageNumber, pageSize);
-            return View(house);
+
+            IQueryable<House> houses = db.Houses;
+            if (!String.IsNullOrEmpty(region))
+            {
+                houses = houses.Where(x => x.Region == region);
+            }
+            if (startPrice != null)
+            {
+                houses = houses.Where(x => x.Price >= startPrice);
+            }
+            if (finishPrice != null)
+            {
+                houses = houses.Where(x => x.Price <= finishPrice);
+            }
+            if (numberOfPersons != null)
+            {
+                houses = houses.Where(x => x.NumberOfPersons >= numberOfPersons);
+            }
+
+            //Определяем максимальную и минимальную цену аренды
+            var Price = db.Houses.Select(x => x.Price);
+            int max = 0;
+            int min = 999;
+            foreach(var price in Price)
+            {
+                if (max < price) max = price;
+                if (min > price) min = price;
+            }
+            ViewBag.MaxPrice = max;
+            ViewBag.MinPrice = min;
+
+            return View(houses.OrderBy(x => x.Id).ToPagedList(pageNumber, pageSize));
         }
 
         public ActionResult About()
