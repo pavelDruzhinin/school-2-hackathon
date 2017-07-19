c26518e49b54c4f28bf53d4c43e80253d5cfe862
MADSEN\svercosh <madsen08@mail.ru>
2017-07-20 00:58:50 +0300

    В раздел ЛК добавил возможность отменить бронь. Отмена доступна до начала дня въезда, после чего переходит в раздел "История поездок"

---
+++ RosCottedge/RosCottedge/ViewModels/MyTripsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using RosCottedge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RosCottedge.ViewModels
+{
+    public class MyTripsViewModel
+    {
+        public IQueryable<Reservation> ReservationHistory { get; set; }
+        public IQueryable<Reservation> ReservationDelete { get; set; }
+
+    }
+}
