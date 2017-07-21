9962e991b4ed6395a4a6528c48c41dc79529cd5f
MADSEN\svercosh <madsen08@mail.ru>
2017-07-22 01:04:28 +0300

    Сортировка уведомлений в ЛК по дате добавления

--- RosCottedge/RosCottedge/ViewModels/EditMyHouseViewModel.cs
+++ RosCottedge/RosCottedge/ViewModels/EditMyHouseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using RosCottedge.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,28 @@ namespace RosCottedge.ViewModels
 {
     public class EditMyHouseViewModel
     {
-        public IQueryable<Review> Reviews { get; set; }
-        public IQueryable<Reservation> Reservations { get; set; }
         public IQueryable<Picture> Pictures { get; set; }
-        public IQueryable<ReservDelNotice> ReservDelNotices { get; set; }
+        public IEnumerable<GeneralСlass> GeneralClass { get; set; }
         public House House { get; set; }
         public User User { get; set; }
     }
+    public class GeneralСlass
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime? ArrivalDate { get; set; }
+        public DateTime? DepartureDate { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int HouseId { get; set; }
+        public House House { get; set; }
+        public bool Tenant { get; set; }
+        public bool Landlord { get; set; }                
+        public string Comment { get; set; }        
+        public float Rating { get; set; }
+        public bool Reserv { get; set; }
+        public bool Review { get; set; }
+    }
+
 }
