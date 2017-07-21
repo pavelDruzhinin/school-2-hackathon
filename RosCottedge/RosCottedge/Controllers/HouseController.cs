c6e0e739c7f8bb266edbba1b5ad419a2499459c2
DESKTOP-TCLD76E\gigdi <hoqpe@yandex.ru>
2017-07-21 18:19:57 +0300

    Добавил infoWindow на карту

--- RosCottedge/RosCottedge/Controllers/HouseController.cs
+++ RosCottedge/RosCottedge/Controllers/HouseController.cs
@@ -177,7 +177,7 @@ namespace RosCottedge.Controllers
 
         public JsonResult HouseObject()
         {
-            var House = db.Houses.Select(n => new { n.lat, n.lng,n.Name }).Where(x => x.lat != null && x.lng != null);
+            var House = db.Houses.Select(n => new { n.lat, n.lng,n.Name,n.Id,n.Locality,n.Area, n.NumberOfPersons,n.Price,n.Rating }).Where(x => x.lat != null && x.lng != null);
             return Json(House, JsonRequestBehavior.AllowGet);
         }
 
